ac7bea04a18f7f746148e6d58b1c0dc6d6ce23ec
SuperMoreh <super.moreh@gmail.com>
2025-12-29 19:25:28 -0300

    Fix Express 5 wildcard syntax

--- Back-INTEC/src/index.ts
+++ Back-INTEC/src/index.ts
@@ -57,7 +57,7 @@ app.use('/api', terminationRouter);
 
 // Serve Frontend Static Files
 app.use(express.static(path.join(__dirname, '../public')));
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
